9371f3df7e1bfa3be3ab27c7fcaefd97cc7aaf16
gerzele <g.a.zelenov@gmail.com>
2021-12-13 21:28:21 +0300

    Working on shared storage assigment

--- debextract.sh
+++ debextract.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 #docker volume create --name debexports
-containerID=$(docker run --detach magnetikonline/buildnginx)
-docker cp "$containerID:/debexports" "/Users/german/DevOPS/task1/"
-sleep 1
-docker rm "$containerID"
+#containerID=$(docker run --detach XXXX)
+#docker cp "$containerID:/debexports" "/Users/german/DevOPS/task1/"
+#sleep 1
+#docker rm "$containerID"
+
+docker run -d \
+  --name=myimg:builder1 \
+  --mount source=debexports,destination=/debexport
