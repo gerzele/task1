466215c9d25209ca94beae715173bcfab8ba5e6e
gerzele <g.a.zelenov@gmail.com>
2021-12-15 23:16:55 +0300

    Installation scripts + DEB package folder structure

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM debian:latest
 VOLUME /debexport
-#RUN apt-get update && apt-get upgrade --yes && \
-#	apt-get install --yes checkinstall && \
-#	apt-get clean
+RUN apt-get update && apt-get upgrade --yes && \
+	apt-get install dpkg-deb --yes checkinstall && \
+	apt-get clean
 
-COPY ./sources/PatchManagerPlus.tgz /tmp/build/
+#COPY ./sources/PatchManagerPlus.tgz /tmp/build/
+COPY ./sources/*.tgz /tmp/build/
 WORKDIR /tmp/build
-RUN tar -xf PatchManagerPlus.tgz
+RUN tar -xf *.tgz
 #RUN tar -xf PatchManagerPlus.tgz --wildcards "*.bin" "*json"
 
 COPY /debmaker.sh /tmp/build/
--- debmaker.sh
+++ debmaker.sh
@@ -1,5 +1,59 @@
 #!/bin/bash
 
 #Volume testing template
-ls -l /tmp/build/ > /debexport/message
-ls -l /debexport > /debexport/message2
+#ls -l /tmp/build/ > /debexport/message
+#ls -l /debexport > /debexport/message2
+
+#Check if we have working directory available, create deb package folder structure, copy source files
+if [[ -d $BasePath=/tmp/build/PatchManagerPlus/ ]]
+then
+    cd $BasePath
+    mkdir -p /PatchManagerPlus_1.0-1_arm64/DEBIAN/ && mkdir -p /PatchManagerPlus_1.0-1_arm64/tmp/packageinstaller/
+    cp *.{bin,json} /PatchManagerPlus_1.0-1_arm64/tmp/packageinstaller/
+    cd $DebPackagePath=/tmp/build/PatchManagerPlus/DEBIAN/
+fi
+
+#===
+#Create internal scripts and configuration files for deb package
+#===
+if [[ -f $DebPackagePath/control ]]
+then
+    cat <<EOF > control
+        Package: PatchManagerPlus
+        Version: 1.0 
+        Section: base 
+        Priority: optional 
+        Architecture: amd64 
+        Maintainer: vendor@vendordomain.com
+        Description: Package Manager
+EOF
+fi
+
+#Adding pre\post installation scripts
+if [[ -f $DebPackagePath/preinst ]]
+    cat <<EOF > 
+#Preinstallation checks here
+#!/bin/bash
+set -e
+
+if systemctl is-active --quiet dcservice
+then
+    systemctl stop dcservice
+else   
+    echo 'Nothing to stop'
+fi
+EOF
+fi
+
+if [[ -f $DebPackagePath/postinst ]]
+    cat <<EOF > 
+#Postinstallation steps here
+set -e
+chmod +x PatchManagerPlus_LinuxAgent.bin
+EOF
+fi
+
+
+#Template to bild the package
+#dpkg-deb --build --root-owner-group $BasePath/PatchManagerPlus_1.0-1_arm64
+
